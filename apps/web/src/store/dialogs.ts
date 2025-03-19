51992aac98b67edece8784ab693724922cbc9f62
sarthakskumar <sarthakskumar7@gmail.com>
2025-03-19 05:43:40 +0530

    chore: delete existing files

--- apps/web/src/store/dialogs.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from 'jotai'
-
-type Dialogs = {
-  signIn: boolean
-}
-
-export const dialogsAtom = atom<Dialogs>({
-  signIn: false
-})
--- apps/web/src/store/highlighter.ts
+++
@@ -1,5 +0,0 @@
-import type { HighlighterCore } from 'shiki'
-
-import { atom } from 'jotai'
-
-export const highlighterAtom = atom<HighlighterCore | null>(null)
